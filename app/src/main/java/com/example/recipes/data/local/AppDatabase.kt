788d89b1ac75656218c7d8bab65c3ca3bd5c28cc
ivan <timofeevi579@gmail.com>
2026-02-18 20:23:44 +0300

    created database

--- app/src/main/java/com/example/recipes/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/recipes/data/local/AppDatabase.kt
@@ -1,4 +1,17 @@
 package com.example.recipes.data.local
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.recipes.data.local.dao.RecipeDao
+import com.example.recipes.data.local.dao.TagDao
+import com.example.recipes.data.local.entities.Recipe
+import com.example.recipes.data.local.entities.RecipeTagCross
+import com.example.recipes.data.local.entities.Tag
+
+@Database(entities = [Recipe::class, Tag::class, RecipeTagCross::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun recipeDao() : RecipeDao
+    abstract fun tagDao() : TagDao
+
+
 }
