df9a9ff25a4f16427a263504db8809add4220d08
ivan <timofeevi579@gmail.com>
2026-03-02 23:28:05 +0300

    enhanced dao functions (changed a return value to Flow<List<RecipeWithTags>> for more flowless experience and added new functions)

--- app/src/main/java/com/example/recipes/data/local/dao/RecipeDao.kt
+++ app/src/main/java/com/example/recipes/data/local/dao/RecipeDao.kt
@@ -4,8 +4,13 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
 import androidx.room.Update
 import com.example.recipes.data.local.entities.Recipe
+import com.example.recipes.data.local.entities.RecipeTagCross
+import com.example.recipes.data.local.entities.Tag
+import com.example.recipes.data.local.relations.RecipeWithTags
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RecipeDao {
@@ -15,8 +20,22 @@ interface RecipeDao {
     @Query("SELECT * FROM recipes WHERE LOWER(title) LIKE LOWER('%' || :search || '%')")
     suspend fun findByTitle(search: String) : List<Recipe>
 
+    @Transaction
+    @Query("SELECT * FROM recipes")
+    fun getRecipesWithTags(): Flow<List<RecipeWithTags>>
+
     @Insert
-    suspend fun insertRecipe(recipe: Recipe)
+    suspend fun insertRecipe(recipe: Recipe): Long
+
+    @Insert
+    suspend fun insertRecipeTags(crossRef: List<RecipeTagCross>)
+
+    @Transaction
+    suspend fun insertRecipeWithTags(recipe: Recipe, tags: List<Tag>) {
+        val recipeId = insertRecipe(recipe)
+        val crossRefs = tags.map { RecipeTagCross(recipeId, it.tid) }
+        insertRecipeTags(crossRefs)
+    }
 
     @Delete
     suspend fun deleteRecipe(vararg recipe: Recipe)
