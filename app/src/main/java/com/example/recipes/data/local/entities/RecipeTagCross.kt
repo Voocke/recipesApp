361225975b621edc13c84f607932e375903bf69a
ivan <timofeevi579@gmail.com>
2026-03-03 19:09:33 +0300

    implemented cascade delete

--- app/src/main/java/com/example/recipes/data/local/entities/RecipeTagCross.kt
+++ app/src/main/java/com/example/recipes/data/local/entities/RecipeTagCross.kt
@@ -2,10 +2,32 @@ package com.example.recipes.data.local.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 
 
-@Entity(tableName = "recipe_tag_cross", primaryKeys = ["recipe_id", "tag_id"])
+@Entity(
+    tableName = "recipe_tag_cross",
+    primaryKeys = ["rid", "tid"],
+    indices = [
+        Index(value = ["tid"])
+    ],
+    foreignKeys = [
+        ForeignKey(
+            entity = Recipe::class,
+            parentColumns = ["rid"],
+            childColumns = ["rid"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Tag::class,
+            parentColumns = ["tid"],
+            childColumns = ["tid"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class RecipeTagCross(
-    @ColumnInfo(name = "recipe_id") val recipeId: Long,
-    @ColumnInfo(name = "tag_id") val tagId: Long
+    @ColumnInfo(name = "rid") val recipeId: Long,
+    @ColumnInfo(name = "tid") val tagId: Long
 )
