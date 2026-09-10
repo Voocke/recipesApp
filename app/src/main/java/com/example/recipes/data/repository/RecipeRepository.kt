1b87e91768066adbf8c2f16515d6a8146d2ba886
voocke <timofeevi579@gmail.com>
2026-09-10 13:55:06 +0300

    added missing dao functions (update & delete)

--- app/src/main/java/com/example/recipes/data/repository/RecipeRepository.kt
+++ app/src/main/java/com/example/recipes/data/repository/RecipeRepository.kt
@@ -1,36 +1,33 @@
 package com.example.recipes.data.repository
 
 import com.example.recipes.data.local.dao.RecipeDao
-import com.example.recipes.data.local.dao.TagDao
 import com.example.recipes.data.local.entities.Recipe
-import com.example.recipes.data.local.entities.RecipeTagCross
 import com.example.recipes.data.local.entities.Tag
 import com.example.recipes.data.local.relations.RecipeWithTags
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
-class RecipeRepository @Inject constructor (private val recipeDao: RecipeDao, private val tagDao: TagDao) {
+class RecipeRepository @Inject constructor (private val recipeDao: RecipeDao) {
 
 
-    fun getAllRecipes(): Flow<List<RecipeWithTags>> {
+    fun getRecipesWithTags(): Flow<List<RecipeWithTags>> {
         return recipeDao.getRecipesWithTags()
     }
-
-    suspend fun searchRecipes(query: String): List<Recipe> {
+    suspend fun searchRecipesByTitle(query: String): List<Recipe> {
         return recipeDao.findByTitle(query)
     }
-
     suspend fun insertRecipe(recipe: Recipe) {
         recipeDao.insertRecipe(recipe)
     }
-
-    suspend fun insertTag(tag: Tag) {
-        tagDao.insertTag(tag)
+    suspend fun insertRecipeWithTags(recipe: Recipe, tags: List<Tag>){
+        recipeDao.insertRecipeWithTags(recipe,tags)
     }
-
-    suspend fun addTagToRecipe(recipeId: Long, tagId: Long) {
-        tagDao.insertRecipeTag(RecipeTagCross(recipeId, tagId))
+    suspend fun deleteRecipe(vararg recipe: Recipe){
+        recipeDao.deleteRecipe(*recipe)
+    }
+    suspend fun updateRecipe(recipe: Recipe){
+        recipeDao.updateRecipe(recipe)
     }
 
 
